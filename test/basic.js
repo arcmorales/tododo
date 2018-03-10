43819142d4550dc6b8a8987e6cab1366d80ebfad
ara <arcmorales86@gmail.com>
2018-03-10 14:36:37 +0800

    try: updated basic.js using dweetAPI

--- test/basic.js
+++ test/basic.js
@@ -5,25 +5,75 @@ var chakram = require('chakram'),
         reporter: 'mochawesome'
       });
 
-describe("Basic test", function () {
+describe("New basic test", function () {
     before("Initializing headers", function () {
-        chakram.setRequestDefaults({
-            strictSSL: false
-        });
+        // chakram.setRequestDefaults({
+        //     strictSSL: false
+        // });
+
+        specifiedThingName = 'chakram-test-thing';
+        initialDweetData = {
+            description: "test sending a string",
+            sensorValue: 0.2222,
+            alert: true
+        };
+        namedDweetPost = chakram.post("https://dweet.io/dweet/for/"+specifiedThingName, initialDweetData);
+   
     });
 
-    it("should do a basic get test", function () {
-        return getStat = chakram.get("", )
-            .then(function (searchResponse) {
-                expect(searchResponse).to.have.status(200);
-                console.log(searchResponse)
-                return chakram.wait();
-            });
+    afterEach("Log test result", function () {
+        var logger = [
+        {
+            "describe": this.currentTest.parent.title,
+            "it_block": this.currentTest.title,
+            "test_state": this.currentTest.state,
+            "test_duration": this.currentTest.duration
+        }]
+
+    })
+
+
+    it("should return 200 on success", function () {
+        return expect(namedDweetPost).to.have.status(200);
     });
-    it("should do a basic get test - not promised", function () {
-        var getStat = chakram.get("", )
-        return expect(getStat).to.have.status(400);
-        console.log(getStat)
+    
+    it("should specify success in the response 'this' field", function () {
+        return expect(namedDweetPost).to.have.json('this', 'succeeded');
+    });
+    
+    it("should respond with the created dweet's data", function () {
+        return expect(namedDweetPost).to.have.json('with.content', initialDweetData);
+    });
+
+    it("should use a dweet thing name if provided", function () {
+        return expect(namedDweetPost).to.have.json('with.thing', specifiedThingName);
+    });
+    
+    it("should allow retrieval of the last data point", function () {
+        var dataRetrieval = chakram.get("https://dweet.io/get/latest/dweet/for/"+specifiedThingName);
+        return expect(dataRetrieval).to.have.json('with[0].content', initialDweetData);
+    });
+    
+    it("should respond with data matching the dweet schema", function () {
+        var expectedSchema = {
+            type: "object",
+            properties: {
+                this: {type: "string"},
+                by: {type: "string"},
+                the: {type: "string"},
+                with: {
+                    type: "object",
+                    properties: {
+                        thing: {type: "string"},
+                        created: {type: "string"},
+                        content: {type: "object"}
+                    },
+                    required: ["thing", "created", "content"]
+                }
+            },
+            required: ["this", "by", "the", "with"]
+        };
+        return expect(namedDweetPost).to.have.schema(expectedSchema);
     });
 
 });
