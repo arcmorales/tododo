45994dc377dc4725a2fa422aa32170b7878e2810
ara <arcmorales86@gmail.com>
2018-03-15 00:24:43 +0800

    experiment on field names with dot notations

--- test/schema.js
+++ test/schema.js
@@ -1,76 +1,79 @@
 var chakram = require('chakram'),
-    expect = chakram.expect;
+  expect = chakram.expect;
 
-describe("Schema test", function () {
+describe("Schema test", function() {
 
-    var mockedSchema;
+  var mockedSchema, partSchema;
 
-    before("Define reference schema", function () {
-       mockedSchema = {
-        type: "object",
-        properties: {
-            "pv.irradiation": {
-                type: "object",
-                properties: {
-                    "unit" : {type: "string"},
-                    "timestamp": {type: "number"},
-                    "value" : {type: "number"}
-                }
-            },
-             "pvinv.activepower": {
-                type: "object",
-                properties: {
-                    "unit" : {type: "string"},
-                    "timestamp": {type: "number"},
-                    "value" : {type: "number"}
-                }
-            },
-            "pvinv.energy": {
-                type: "object",
-                properties: {
-                    "unit" : {type: "string"},
-                    "timestamp": {type: "number"},
-                    "value" : {type: "number"}
-                }
-            }
+  before("Define reference schema", function() {
+
+    mockedSchema = {
+      type: "object",
+      properties: {
+        "pv.irradiation": {
+          type: "object",
+          properties: {
+            "unit": { type: "string" },
+            "timestamp": { type: "number" },
+            "value": { type: "number" }
+          }
+        },
+        "pvinv.activepower": {
+          type: "object",
+          properties: {
+            "unit": { type: "string" },
+            "timestamp": { type: "number" },
+            "value": { type: "number" }
+          }
+        },
+        "pvinv.energy": {
+          type: "object",
+          properties: {
+            "unit": { type: "string" },
+            "timestamp": { type: "number" },
+            "value": { type: "number" }
+          }
         }
-       }
-    })
+      }
+    };
 
-
-
-    it("should return 200 on success", function () {
+    partSchema = {
+          type: "object",
+          properties: {
+            "unit": { type: "string" },
+            "timestamp": { type: "number" },
+            "value": { type: "boolean" }
+          }
         
-        return chakram.get("http://demo9405120.mockable.io/power")
-        .then(function (chalia) {
-            expect(chalia).to.have.status(200);      
-        })
-    });
-    
-    // it("should specify success in the response 'this' field", function () {
-    //     return expect(namedDweetPost).to.have.json('this', 'succeeded');
-    // });
-    
-    // it("should respond with the created dweet's data", function () {
-    //     return expect(namedDweetPost).to.have.json('with.content', initialDweetData);
-    // });
+    };
 
-    // it("should use a dweet thing name if provided", function () {
-    //     return expect(namedDweetPost).to.have.json('with.thing', specifiedThingName);
-    // });
-    
-    // it("should allow retrieval of the last data point", function () {
-    //     var dataRetrieval = chakram.get("https://dweet.io/get/latest/dweet/for/"+specifiedThingName);
-    //     return expect(dataRetrieval).to.have.json('with[0].content', initialDweetData);
-    // });
-    
-    it("should respond with data matching schema", function (){
-        return chakram.get("http://demo9405120.mockable.io/power")
-        .then(function (chalia) {
-            expect(chalia).to.have.schema(mockedSchema);
-        })
-        
-    });
+
+  });
+
+
+  it("should return 200 on success", function() {
+
+    return chakram.get("http://demo9405120.mockable.io/power")
+      .then(function(chalia) {
+        expect(chalia).to.have.status(200);
+      })
+  });
+
+
+  it("should respond with data matching schema", function() {
+    return chakram.get("http://demo9405120.mockable.io/power")
+      .then(function(chalia) {
+        expect(chalia).to.have.schema(mockedSchema);
+      })
+
+  });
+
+  it("should respond with data matching the child property of the schema", function() {
+    let item = `pvinv\.energy`;
+     return chakram.get("http://demo9405120.mockable.io/power")
+      .then(function(chalia) {
+        expect(chalia).to.have.schema(item, partSchema);
+      })
+  });
 
 });
-
